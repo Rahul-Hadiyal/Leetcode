51022ea2f31acf2fb0dc20bc30c2df25d28fef1e
Rahul-Hadiyal <rahulhadiyal549@gmail.com>
2025-08-10 16:55:27 +0530

    Add files via upload

---
+++ Array/#034 Find-First-and-Last-Index.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector <int> res;
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]==target){
+                res.push_back(i);
+                break; 
+            }
+        }
+        for (int j=nums.size()-1;j>=0;j--){
+            if(nums[j]==target){
+                res.push_back(j);
+                break;
+            }
+        }
+        if (res.size()==0){
+            return {-1,-1};
+        }else{
+            return res;
+        }
+    }
+};
